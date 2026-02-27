5edbf6c8c360a4339f43c5b3912a3077a12ac7d9
Mikael Ståldal <mikael@staldal.nu>
2026-02-27 17:43:22 +0100

    Remove request logging

--- internal/handler/middleware.go
+++ internal/handler/middleware.go
@@ -3,19 +3,10 @@ package handler
 import (
 	"log"
 	"net/http"
-	"time"
 )
 
 func withMiddleware(h http.Handler) http.Handler {
-	return recoveryMiddleware(loggingMiddleware(corsMiddleware(h)))
-}
-
-func loggingMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		start := time.Now()
-		next.ServeHTTP(w, r)
-		log.Printf("%s %s %s", r.Method, r.URL.Path, time.Since(start))
-	})
+	return recoveryMiddleware(corsMiddleware(h))
 }
 
 func recoveryMiddleware(next http.Handler) http.Handler {
