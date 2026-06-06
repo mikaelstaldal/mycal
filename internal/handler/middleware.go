9d4d155c7bf504149cb1827cc71784c79adcadfc
Mikael Ståldal <mikael@staldal.nu>
2026-06-06 18:32:52 +0200

    Extract common code to library

--- internal/handler/middleware.go
+++ internal/handler/middleware.go
@@ -1,17 +1,14 @@
 package handler
 
 import (
-	"compress/gzip"
-	"io"
 	"net/http"
-	"strings"
 
 	"github.com/mikaelstaldal/go-server-common/httputil"
 	"github.com/mikaelstaldal/go-server-common/recovery"
 )
 
 func withMiddleware(h http.Handler) http.Handler {
-	return recovery.Middleware(gzipMiddleware(apiCacheMiddleware(h)))
+	return recovery.Middleware(httputil.Gzip(apiCacheMiddleware(h)))
 }
 
 // apiCacheMiddleware prevents caching of dynamic API responses.
@@ -19,33 +16,6 @@ func apiCacheMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Cache-Control", "no-store")
 		next.ServeHTTP(w, r)
-	})
-}
-
-type gzipResponseWriter struct {
-	http.ResponseWriter
-	writer io.Writer
-}
-
-func (g *gzipResponseWriter) Write(b []byte) (int, error) {
-	return g.writer.Write(b)
-}
-
-func gzipMiddleware(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
-			next.ServeHTTP(w, r)
-			return
-		}
-		gz, err := gzip.NewWriterLevel(w, gzip.DefaultCompression)
-		if err != nil {
-			next.ServeHTTP(w, r)
-			return
-		}
-		defer func() { _ = gz.Close() }()
-		w.Header().Set("Content-Encoding", "gzip")
-		w.Header().Del("Content-Length")
-		next.ServeHTTP(&gzipResponseWriter{ResponseWriter: w, writer: gz}, r)
 	})
 }
 
